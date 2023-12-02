ae72e66e8772a1af75b6ce0b140a9aa12065a116
Natalkalvivyanka <nataliya.zahayska@testdevlab.com>
2023-12-02 15:19:58 +0100

    created registration flow

---
+++ features/support/data/userData.js
@@ -0,0 +1,31 @@
+export default class UserData{
+
+    constructor(){
+        this.newUser = new NewUser(
+            'female',
+            'Nataliya',
+            'Zahayska',
+            '7',
+            '8',
+            '1989',
+            'natalka.zahayska@testdevlab.com',
+            'tdl',
+            'Newspage@1234simba'
+        )
+    }
+
+}
+
+class NewUser{
+    constructor(gender, firstName, lastName, birthDay, birthMonth, birthYear, email, company, password){
+        this.gender = gender;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDay = birthDay;
+        this.birthMonth = birthMonth;
+        this.birthYear = birthYear;
+        this.email = email;
+        this.company = company;
+        this.password = password
+    }
+}
